89a7bb856542c5f693c1969b4fb6b4f685dbe983
scespinoza <secancinoe@gmail.com>
2022-10-10 12:01:05 -0300

    fix readme and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,16 @@ import "./index.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <Text align="center">built by Sebastián C. Espinoza</Text>
+      <Text align="center">
+        built by{" "}
+        <a
+          href="https://github.com/scespinoza/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Sebastián C. Espinoza
+        </a>
+      </Text>
       <Text align="center">
         Data provided by the{" "}
         <a href="https://oec.world/" target="_blank" rel="noreferrer">
